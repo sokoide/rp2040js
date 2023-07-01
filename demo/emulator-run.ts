9dffd4ca27e2708f8ca7d965e111f0759fccc1b0
So Koide <sokoide@gmail.cojm>
2023-07-01 17:48:19 +0900

    add: SIGHUP to restart gracefully

--- demo/emulator-run.ts
+++ demo/emulator-run.ts
@@ -6,17 +6,38 @@ import { GDBTCPServer } from '../src/gdb/gdb-tcp-server';
 
 // Create an array with the compiled code of blink
 // Execute the instructions from this array, one by one.
-const hex = fs.readFileSync('hello_uart.hex', 'utf-8');
-const mcu = new RP2040();
-mcu.loadBootrom(bootromB1);
-loadHex(hex, mcu.flash, 0x10000000);
+
+let mcu = new RP2040();
+
+function reloadHex() {
+  let hexFile = process.env['picotarget'];
+  if (hexFile === undefined) {
+    hexFile = 'hello_uart.hex';
+  }
+  const hex = fs.readFileSync(hexFile, 'utf-8');
+  mcu.loadBootrom(bootromB1);
+  loadHex(hex, mcu.flash, 0x10000000);
+}
+
+function restart() {
+  reloadHex();
+  mcu.uart[0].onByte = (value) => {
+    process.stdout.write(new Uint8Array([value]));
+  };
+  mcu.core.PC = 0x10000000;
+  mcu.execute();
+}
 
 const gdbServer = new GDBTCPServer(mcu, 3333);
 console.log(`RP2040 GDB Server ready! Listening on port ${gdbServer.port}`);
 
-mcu.uart[0].onByte = (value) => {
-  process.stdout.write(new Uint8Array([value]));
-};
+process.on('SIGHUP', () => {
+  console.log('SIGHUP');
+  mcu.stop();
+  // mcu.uart[0].onByte = () => {};
 
-mcu.core.PC = 0x10000000;
-mcu.execute();
+  mcu = new RP2040();
+  restart();
+});
+
+restart();
